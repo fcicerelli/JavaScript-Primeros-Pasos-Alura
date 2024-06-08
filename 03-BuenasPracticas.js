57a0dad1b4d20130d016c00920a1efeb4739586e
fcicerelli <fcicerellitic@gmail.com>
2024-06-08 09:23:39 -0300

    ejercicios 03 y 04

---
+++ 03-BuenasPracticas.js
@@ -0,0 +1,11 @@
+let nombreEmpleado = "José Perez";
+let NombreEmpleado = "Pedro Ramírez";
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
+
+let edadEmpleado = 40;
+console.log(edadEmpleado);
+edadEmpleado = "Cuarenta";
+console.log(edadEmpleado);
+
